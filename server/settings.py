6ebca4e3107b10a3d6ff8b1047e2f49a7ad44b16
Tahir Ali <tahirbacha506@gmail.com>
2026-01-15 19:07:10 +0500

    Added exception handler

--- server/settings.py
+++ server/settings.py
@@ -42,7 +42,8 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'EXCEPTION_HANDLER': 'accounts.exceptions.custom_exception_handler'
 }
 
 ROOT_URLCONF = 'server.urls'
