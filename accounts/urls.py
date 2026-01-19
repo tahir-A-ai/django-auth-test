8f60ad7af70ecd3c9fdf3a986ebfd343ebdd2b2d
Tahir Ali <tahirbacha506@gmail.com>
2026-01-19 16:19:33 +0500

    update urls structure

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import SignUpView, LoginView, MyProfileView, EditProfileView
 
-
-auth_patterns = [
-    path('sign-up/', SignUpView.as_view(), name='sign_up'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
+urlpatterns = [
+    path('auth/sign-up/', SignUpView.as_view(), name='sign_up'),
+    path('auth/login/', LoginView.as_view(), name='login'),
+    path('auth/refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', MyProfileView.as_view(), name='my_profile'),
+    path('profile/edit/', EditProfileView.as_view(), name='edit_profile'),
 ]
