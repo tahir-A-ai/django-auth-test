0f110b2e6d38508a79954f01762cb8b4e013eaab
Tahir Ali <tahirbacha506@gmail.com>
2026-01-19 11:16:19 +0500

    Modified fields

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, default='')
+    name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=254, unique=True)
+    phone = models.CharField(max_length=16, default='', blank=True)
     address = models.TextField(blank=True, null=True)
 
     USERNAME_FIELD = 'email'
